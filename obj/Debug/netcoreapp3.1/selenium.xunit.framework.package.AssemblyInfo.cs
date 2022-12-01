21da22fd03d3c9655866988583225099d6b65430
Varsha Jankar <varsha.jankar@reisystems.com>
2022-12-01 01:17:58 -0500

    framework updated to generate Nuget package

--- obj/Debug/netcoreapp3.1/selenium.xunit.framework.package.AssemblyInfo.cs
+++ obj/Debug/netcoreapp3.1/selenium.xunit.framework.package.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("selenium.xunit.framework.package")]
+[assembly: System.Reflection.AssemblyProductAttribute("External libraries")]
 [assembly: System.Reflection.AssemblyTitleAttribute("selenium.xunit.framework.package")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
